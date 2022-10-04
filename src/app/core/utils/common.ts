565729df5caf2cef7fb6abc659be6ee93143a274
ThacHungNguyen <nthachung@gmail.com>
2022-10-04 23:29:33 +0700

    update

--- src/app/core/utils/common.ts
+++
@@ -1,8 +0,0 @@
-export class commonUtils {
-  static convertArrayToObjectByID(array: any[]) {
-    return array.reduce((object, cur) => {
-      object[cur["id"]] = cur;
-      return object;
-    });
-  }
-}

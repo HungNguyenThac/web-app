f580ffbc83133c82bc590420c20c16d9528408c6
ThacHungNguyen <nthachung@gmail.com>
2022-10-08 23:25:00 +0700

    update

--- src/app/layout/index.ts
+++
@@ -1,5 +0,0 @@
-import { HeaderComponent } from "@app/layout/header/header.component";
-import { FooterComponent } from "@app/layout/footer/footer.component";
-import { SidebarComponent } from "@app/layout/sidebar/sidebar.component";
-
-export const layout: any[] = [HeaderComponent, FooterComponent, SidebarComponent];
